1220a1ac59aa225cc551a9e510571db31ab4ab23
Femi Akindoyin <72978200+femi-akins@users.noreply.github.com>
2023-08-25 16:58:02 +0100

    Updated files

--- js/index.js
+++ js/index.js
@@ -18,3 +18,19 @@ function openCity(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+
+if (close) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
